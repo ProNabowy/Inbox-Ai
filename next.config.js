aeda256f7b8df6270d8c8d1f6646bba9f9cc7b99
ProNabowy <96296770+ProNabowy@users.noreply.github.com>
2025-08-08 10:50:30 +0300

    comment rewrites

--- next.config.js
+++ next.config.js
@@ -1,38 +1,28 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-	reactStrictMode: true,
-	compress: true,
-	experimental: {
-		useLightningcss: false,
-	},
-	compiler: {
-		styledComponents: true,
-	},
-	rewrites: async () => {
-		return [
-			{
-				source: "/backend/API/:path*",
-				destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-			},
-		];
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "**.amazonaws.com",
-				port: "",
-			},
-			{
-				protocol: "https",
-				hostname: "biddex-app-v2.test",
-				port: "",
-			},
-			{
-				protocol: "https",
-				hostname: "**.biddex.info",
-				port: "",
-			},
-		],
-	},
+  reactStrictMode: true,
+  compress: true,
+  experimental: {
+    useLightningcss: false,
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  rewrites: async () => {
+    return [
+      {
+        // source: "/backend/API/:path*",
+        // destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.amazonaws.com",
+        port: "",
+      },
+    ],
+  },
 };
